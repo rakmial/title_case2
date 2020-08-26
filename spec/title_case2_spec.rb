b9acc0204199b69b0c607d1695e11be372c0af24
Joshua Lovelace <rakmial@gmail.com>
2020-08-25 17:02:13 -0700

    test 2 red

--- spec/title_case2_spec.rb
+++ spec/title_case2_spec.rb
@@ -1,19 +1,23 @@
 require('rspec')
 require('title_case2')
 
-'''
-Requirements: Create a method on the String clss that takes a string
-of word(s) from user and returns it in title case.
-  
-  Example: "gore vidal" > "Gore Vidal"
-  
-  1. Define the method named "title_case" under class String
-  2. String.title_case returns of words array via split
-  3. Loop through words to .capitalize!
-  4. Join array on " "
-  '''
-  describe("title_case2") do
-    it('must define method String.title_case that returns already capitalized strings the same') do
-      expect("Cat".title_case).to(eq("Cat"))  
-    end
+
+# Requirements: Create a method on the String clss that takes a string
+# of word(s) from user and returns it in title case.
+
+# Example: "gore vidal" > "Gore Vidal"
+
+# 1. String.title_case must be defined, and will return already capitalized words as they are.
+# 2. String.title_case returns single word with uncapitalized beginning in title case
+# 3. String.title_case returns multiple words in a string separated by spaces in title case
+
+
+describe("title_case2") do
+  it('must define method String.title_case that returns already capitalized strings the same') do
+    expect("Cat".title_case).to(eq("Cat"))  
   end
+  it('returns uncapitalized single word strings in title case') do
+    expect("cat".title_case).to(eq("Cat"))
+  end
+  # it('returns multi word strings with space separators with each word in title case')
+end
